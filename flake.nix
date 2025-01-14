09b8729b28d57d64e35b693332c90fe5bd86e41e
Sönke Hahn <soenkehahn@gmail.com>
2025-01-13 21:13:43 -0500

    Switch to garnix rust module

--- flake.nix
+++ flake.nix
@@ -1,51 +1,16 @@
 {
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
-    flake-utils.follows = "cargo2nix/flake-utils";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
+    garnix-lib.url = "github:garnix-io/garnix-lib";
+    rust-module.url = "github:garnix-io/rust-module";
   };
-  outputs = { nixpkgs, flake-utils, cargo2nix, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ cargo2nix.overlays.default ];
-        };
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.75.0";
-          packageFun = import ./Cargo.nix;
-          extraRustComponents = [ "clippy" ];
-        };
-      in
-      rec {
-        packages = {
-          porc = rustPkgs.workspace.porc { };
-          default = packages.porc;
-          generateCargoNix = pkgs.writeShellApplication
-            {
-              name = "generateCargoNix";
-              runtimeInputs = [ cargo2nix.packages.${system}.default ];
-              text = ''
-                cargo2nix . --overwrite --locked
-              '';
-            };
-        };
-        checks = {
-          test = pkgs.rustBuilder.runTests rustPkgs.workspace.porc {
-            testCommand = bin:
-              ''
-                export INSTA_WORKSPACE_ROOT=${./.}
-                ${bin}
-              '';
-          };
-        };
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ pkgs.rust-analyzer pkgs.cargo-insta ];
-        };
-        apps.generateCargoNix = {
-          type = "app";
-          program = pkgs.lib.getExe packages.generateCargoNix;
-        };
-      }
-    );
+  outputs = inputs: inputs.garnix-lib.lib.mkModules {
+    modules = [
+      inputs.rust-module.garnixModules.default
+    ];
+    config = { pkgs, ... }: {
+      rust.default = {
+        src = ./.;
+      };
+    };
+  };
 }
